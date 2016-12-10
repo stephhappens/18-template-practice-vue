319ed2fb0054592a1296a661ab17179705683b92
stephhappens <stephanie.n.provence@gmail.com>
2016-12-10 12:17:09 -0600

    RYAN THIS IS DONE

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,61 +1,84 @@
 <template lang="html">
-  <div class="">
-    <nav class="panel">
+    <div class="section">
+    <div class="container">
+
+    <form class="panel" @submit.prevent="submitForm">
       <p class="panel-heading">
         Sign Up For My Web App
       </p>
 
-      <p class="control">
-      <input class="input" type="text" placeholder="First Name">
-        </p>
 
-        <p class="control">
-        <input class="input" type="text" placeholder="Last Name">
-          </p>
+    <div class="panel-block">
+      <p class="control has-icon" v-for= "input in formInputs">
+      <template v-if="input.type === 'select'" v-model="formValues[input.id]">
+        <span class="select is-fullwidth">
+    <select>
+        <option v-for="option in input.options"> {{ option.label }} </option>
+    </select>
+  </span>
+      </template>
 
-        <p class="control">
-        <input class="input" type="text" placeholder="Email Address">
-          </p>
+      <template v-else>
+        <template v-if="input.type === 'textarea'">
+          <textarea class="input textarea" :placeholder="input.label" v-model="formValues[input.id]"></textarea>
+        <i :class="input.icon" class="fa" aria-hidden="true"></i>
+        </template>
 
-        <p class="control">
-        <input class="input" type="text" placeholder="Current website url">
-          </p>
+      <template v-else>
+        <input class="input" :placeholder="input.label" v-model="formValues[input.id]">
+        <i :class="input.icon" class="fa" aria-hidden="true"></i>
+      </template>
+    </template>
+  </p>
+</div>
 
-        <p class="control">
-            <span class="select">
-              <select>
-            <option>English</option>
-            <option>Spanish</option>
-              </select>
-            </span>
-        </p>
-
-      <p class="control">
-      <textarea class="textarea" placeholder="Your Comment"></textarea>
-      </p>
-
-      <p class="control">
-      <input class="input" type="text" placeholder="Mobile Number">
-        </p>
-
-      <p class="control">
-      <input class="input" type="text" placeholder="Phone Number">
-        </p>
-
-     <button class="button is-fullwidth">Submit</button>
-
-</nav>
+  <div class="panel-block">
+     <button class="button is-fullwidth is-primary"> Submit </button>
+  </div>
+</form>
+ </div>
  </div>
 </template>
 
+
+
 <script>
+const apiUrl = 'http://json-data.herokuapp.com/forms';
+
+
 export default {
   data() {
-    return {};
+    return {
+      apiUrl: apiUrl,
+      formInputs: [],
+      formValues: {},
+    };
+  },
+
+  mounted() {
+    this.getData();
   },
 
   methods: {
+    getData() {
+      fetch(`${apiUrl}`)
+        .then((r) => r.json())
+        .then((formInputs) => {
+          this.formInputs = formInputs;
+        });
+    },
 
+    submitForm(formValues) {
+      console.log(this.formValues);
+      fetch ('http://tiny-tn.herokuapp.com/collections/form-stephanie', {
+        method: 'POST',
+        headers: {'content-type': 'application/json',},
+        body: JSON.stringify(this.formValues),
+      })
+      .then(() => {
+        this.formValues = {};
+      });
   },
+},
 };
 </script>
